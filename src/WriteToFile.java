3cdd9a42fc6c1b68ae49ce5adbe4a84fc7e63e97
brandtronja@gmail.com <Esra.2016!>
2018-10-29 13:21:18 +0100

    .

--- src/WriteToFile.java
+++ src/WriteToFile.java
@@ -9,5 +9,13 @@ public class WriteToFile {
         PrintWriter outToFile = new PrintWriter(new FileWriter("output.txt"));
         outToFile.println("A little text");
         outToFile.close();
+        // %= skriv ut
+        // 6=skriv ut sex tecken långt/
+        // &6.3d = skriv ut 6 positioner före decimal sen 3 postioner i decimal
+        // %6s = skriv ut 6 postioner av String
+        int i = 2;
+        double d = i/3.0;
+        String s = "Hello";
+        outToFile.printf("Result:%n i = %6d %n d= = %6.3d %n %6s %n", i,d,s);
     }
 }
